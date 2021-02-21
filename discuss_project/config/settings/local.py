5195fb03ff926e54c15f3ecf2663bae7843745ff
senkaPro <isenatpro@gmail.com>
2021-02-21 21:09:37 +0100

    Update local.py

--- discuss_project/config/settings/local.py
+++ discuss_project/config/settings/local.py
@@ -46,6 +46,7 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR / 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
